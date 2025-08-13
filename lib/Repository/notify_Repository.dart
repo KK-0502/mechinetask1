67c6e7806b1fd93f85e12505a3af131ea13e1560
Krishnakumar KP <krishnakumarkanan133@gmail.com>
2025-08-13 14:25:40 +0530

    The unit test not doing but i completed the json parsing in isolates that was completed

--- lib/Repository/notify_Repository.dart
+++ lib/Repository/notify_Repository.dart
@@ -1,23 +1,41 @@
 import 'dart:convert';
-
+import 'dart:isolate';
 import 'package:mechinenoti/Model/notification_Model.dart';
 import 'package:http/http.dart' as http;
 
 class NotifyRepository {
   Future<List<NotificationModel>> fetchNotify() async {
-  final response = await http.get(Uri.parse('https://raw.githubusercontent.com/sayanp23/test-api/main/test-notifications.json'));
-  if (response.statusCode == 200) {
-    final jsonData = jsonDecode(response.body);
-    final data = jsonData['data']; 
-    if (data != null && data is List) {
-      return data.map((item) => NotificationModel.fromJson(item)).toList();
+    final response = await http.get(
+      Uri.parse(
+        'https://raw.githubusercontent.com/sayanp23/test-api/main/test-notifications.json',
+      ),
+    );
+    if (response.statusCode == 200) {
+      // this area parse in background isolates
+      final parseData = await _parseJsonInIsolate(response.body);
+      if (parseData != null && parseData is List) {
+        return parseData
+            .map((item) => NotificationModel.fromJson(item))
+            .toList();
+      } else {
+        return [];
+      }
     } else {
-      return []; 
+      throw Exception('Failed to load notifications');
     }
-  } else {
-    throw Exception('Failed to load notifications');
+  }
+
+  // This code Spawns a background isolates parse JSON
+  Future<dynamic> _parseJsonInIsolate(String jsonString) async {
+    final recivePort = ReceivePort();
+    await Isolate.spawn(_isolateEntry, [recivePort.sendPort, jsonString]);
+    return await recivePort.first;
+  }
+  // This code for run inside the isolate
+  static void _isolateEntry(List<dynamic> args) {
+    SendPort sendPort = args[0];
+    String jsonString = args[1];
+    final jsonData = jsonDecode(jsonString);
+    sendPort.send(jsonData['data']);
   }
 }
-
-}
-
